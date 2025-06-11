4ed8e39c4c6a57a4ebfd3cd633c685e01da683c7
LizzieSKoepsel <136477454+LizzieSKoepsel@users.noreply.github.com>
2025-06-11 14:28:37 -0300

    funcao

--- script.js
+++ script.js
@@ -4,4 +4,31 @@ const caixaAlternativa = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = ["Pergunta1", "Pergunta2";]
+const perguntas = [
+    {
+        enunciado: "Pergunta 1", 
+        alternativas:[
+            "Alternativa 1", 
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1", 
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    };
+];
+  
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+
+}
+
+mostraPergunta()
