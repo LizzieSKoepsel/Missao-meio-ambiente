5694f83ba61a68dfd3b62f877207235e908a89e2
LizzieSKoepsel <136477454+LizzieSKoepsel@users.noreply.github.com>
2025-06-23 16:17:12 -0300

    tentativa6

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 
 function mostraPergunta() {
@@ -53,15 +54,18 @@ function mostraAlternativas() {
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", function () {
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas);
 
     }
 
 }
-
+function respostaSelecionada(opcaoSelecionada){
+            const afirmacoes = opcaoSelecionada.afirmacoes;
+            historiaFinal = afirmacoes;
+            atual++;
+            mostraPergunta();
+       
+}
 
 mostraPergunta();
